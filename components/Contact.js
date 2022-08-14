24f33d69210bd4101515d1486141fe25495fd86f
Abhay Singh Verma <abhaysinghverma291@gmail.com>
2022-08-14 22:50:35 +0530

    1st commit

---
+++ components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+       return (
+              <div className="sec__one">
+                     <h1> Contact Me at "abhaysinghverma291@gmail.com"</h1>
+              </div>
+       )
+}
+
+export default Contact;
